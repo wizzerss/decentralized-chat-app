0699b1628fa5afd01f1cbf9ff131f099595528a5
Haseen mathar <120903114+wizzseen@users.noreply.github.com>
2024-05-29 14:11:10 +0530

    Update index.tsx

--- config/index.tsx
+++ config/index.tsx
@@ -4,7 +4,7 @@ import { cookieStorage, createStorage } from 'wagmi'
 import { mainnet, sepolia } from 'wagmi/chains'
 
 
-export const projectId = process.env.projectId;
+export const projectId = process.env.ProjectId;
 
 if (!projectId) throw new Error('Project ID is not defined')
 
